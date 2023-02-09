98690206693031de04608ba679195f9d4ea84b26
jamesschuler <jameshschuler1@gmail.com>
2023-02-08 23:07:30 -0800

    Idk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,8 @@ import { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "io.jamesschuler.gogosandiego",
   appName: "GoGo! San Diego",
-  webDir: "public",
+  webDir: "dist",
   bundledWebRuntime: false,
-  server: {
-    url: "http://127.0.0.1:5173/",
-  },
 };
 
 export default config;
